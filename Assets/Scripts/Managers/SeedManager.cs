8285ae04a622eec9974a549fac0b92ac28cc7f98
Tharusha Nemantha <nemanathatharusha@gmail.com>
2026-03-22 20:28:51 +0530

    Add interaction prompts and view inventory
    Add interaction prompts and view inventory

--- Assets/Scripts/Managers/SeedManager.cs
+++ Assets/Scripts/Managers/SeedManager.cs
@@ -1,12 +1,55 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class SeedManager : MonoBehaviour
 {
     public static SeedManager Instance { get; private set; }
 
+    [Header("Seed Settings")]
+    public List<SeedData> availableSeeds = new List<SeedData>();
+
     private void Awake()
     {
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
+    }
+
+    public SeedData GetSeedData(string seedName)
+    {
+        return availableSeeds.Find(seed => seed.seedName == seedName);
+    }
+
+    public bool HasSeed(string seedName, int amount)
+    {
+        if (InventoryManager.Instance != null)
+        {
+            return InventoryManager.Instance.HasItem(seedName, ItemType.Seed, amount);
+        }
+        return false;
+    }
+
+    public void AddSeeds(string seedName, int amount)
+    {
+        if (InventoryManager.Instance != null)
+        {
+            InventoryManager.Instance.AddItem(seedName, ItemType.Seed, amount);
+        }
+    }
+
+    public void RemoveSeeds(string seedName, int amount)
+    {
+        if (InventoryManager.Instance != null)
+        {
+            InventoryManager.Instance.RemoveItem(seedName, ItemType.Seed, amount);
+        }
+    }
+
+    public int GetSeedCount(string seedName)
+    {
+        if (InventoryManager.Instance != null)
+        {
+            return InventoryManager.Instance.GetItemAmount(seedName, ItemType.Seed);
+        }
+        return 0;
     }
 }
